27150c57779e891b1e100771e39926d47dab7ac5
irinaisheree <irinastoyanova@abv.bg>
2024-02-15 12:17:49 +0200

    add static register page

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ const router = require('express').Router()
 const homeController = require('./controllers/homeController')
 // const electronicController = require('./controllers/electronicController')
 // const castController = require("./controllers/castController")
-// const authController = require('./controllers/authController')
+const userController = require('./controllers/userController')
 
 router.use(homeController)
 // router.use(electronicController)
 // router.use('/cast', castController)
-// router.use('/auth', authController)
+router.use('/auth', userController)
 
 router.get('*', (req,res) => {
     res.redirect('/404')
