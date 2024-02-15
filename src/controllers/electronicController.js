ae6cd134c9f8afd361aa14518b81aea1eb043880
irinaisheree <irinastoyanova@abv.bg>
2024-02-15 16:11:07 +0200

    changed nav for details buttons

--- src/controllers/electronicController.js
+++ src/controllers/electronicController.js
@@ -32,13 +32,22 @@ router.get('/:electronicId/details', async(req, res) => {
     const electronicId = req.params.electronicId
     try{
         const electronic = await electronicManager.getOneWithOwner(electronicId).lean()
-        const isOwner = electronic.owner && electronic.owner == req.user?._id//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
+
+        const one = electronic.owner._id.toString()
+        const two =  electronic.owner == req.user?._id.toString()
+        const isOwner = electronic.owner._id.toString() == req.user?._id.toString()//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
         // const casts = await castManager.getByIds(movie.casts).lean() //--- only if populate is not used(populates the cast info into the movie with the ref: Cast in the Movie Schema)
+       
+        console.log(isOwner)
         res.render('electronic/details', {electronic, isOwner})
 } catch(error){
     res.status(400).redirect('/404')
 }
 
+})
+
+router.get('/:electronicId/edit', (req, res) => {
+    res.render('electronic/edit')
 })
 
 module.exports = router
